4e5613947a091bcafbb7f49562d1debccfe34a98
russeii <rratcliffe57@gmail.com>
2016-09-11 20:53:58 -0400

    wtf

--- flaskapp.py
+++ flaskapp.py
@@ -3,19 +3,9 @@ import MySQLdb
 from datetime import datetime
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
-
-db=MySQLdb.connect(
-		host="localhost",
-		user="root",
-		passwd="",
-		db="openshift")
-# you must create a Cursor object. It will let
-#  you execute all the queries you need
-cur = db.cursor()
-
-# Use all the SQL you like
-#cur.execute("SELECT * FROM users")
-
+from engine.sql import SQLConnection
+from engine.plaid_test import plaid
+from engine.fitbit import fitbit
 
 
 app = Flask(__name__)
@@ -29,19 +19,38 @@ def index():
 def serveStaticResource(resource):
     return send_from_directory('static/', resource)
 
-@app.route("/test")
+@app.route("/api/test")
 def test():
     return "<strong>It's Alive!</strong>"
 
-@app.route("/add/<username>")
-def add(username):
+@app.route("/api/post")
+def test():
+    return "<strong>ROFL</strong>"
 
-	# cur.execute("INSERT INTO users VALUES (2, %s,5000);" ,[username])
-	
-	# cur.execute("select 1 from users where Username = %s;" , "hello")
+@app.route("/add_plaid/<username>") #must make sure this works with access_id
+def add_plaid(username):
+	#db=SQLConnection()
+	return 0232
+	p=plaid()
+	p.create_user_plaid("emc67","chase","rratcliffe57","***REMOVED***")
+	p.first_time_plaid_transactions("emc67")
 	return username
 
+@app.route("/get_new_events")
+def get_new_events():
+	db=SQLConnection()
+	p=plaid()
+	p.check_plaid_event("emc67")
+	events= db.get_events("emc67")
+	# for items in test:
+	# 	print items['message']
+	return events
 
+@app.route("/add_friend")
+def add_friend():
+	SQLConnection().add_friend("rwr21","emc67")
+
+	
 
 
 
