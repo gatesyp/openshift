b0df35328d26bc6bc09f0ed6d7bfd7c8edb219d0
emersoncloud <emerson.cloud@gmail.com>
2016-09-15 22:29:49 -0400

    Toolbar works with scrolling between the tabs

--- mobile/ToolbarTest/app/src/main/java/com/example/emerson/toolbartest/MainActivity.java
+++ mobile/ToolbarTest/app/src/main/java/com/example/emerson/toolbartest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.emerson.toolbartest;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -8,6 +10,32 @@ import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.View;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,19 +46,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recList = (RecyclerView) findViewById(R.id.my_recycler_view);
-        recList.setHasFixedSize(true);
-
-        LinearLayoutManager llm = new LinearLayoutManager(this);
-        llm.setOrientation(LinearLayoutManager.VERTICAL);
-        recList.setLayoutManager(llm);
+//        RecyclerView recList = (RecyclerView) findViewById(R.id.my_recycler_view);
+//        recList.setHasFixedSize(true);
+//
+//        LinearLayoutManager llm = new LinearLayoutManager(this);
+//        llm.setOrientation(LinearLayoutManager.VERTICAL);
+//        recList.setLayoutManager(llm);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
 
-        ContactAdapter ca = new ContactAdapter(createList(30));
-        recList.setAdapter(ca);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new SampleFragmentPagerAdapter(getSupportFragmentManager(),
+                MainActivity.this));
+//        viewPager.setOnPageChangeListener(
+//                new ViewPager.SimpleOnPageChangeListener() {
+//                    @Override
+//                    public void onPageSelected(int position) {
+//                        getActionBar().setSelectedNavigationItem(position);
+//                    }
+//                }
+//        );
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+//        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+//        if (viewPager != null) {
+//            setupViewPager(viewPager);
+//        }
+//
+//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+//        tabLayout.setupWithViewPager(viewPager);
+//
+//        ContactAdapter ca = new ContactAdapter(createList(30));
+//        recList.setAdapter(ca);
 
 
     }
@@ -58,4 +110,42 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+
+    //PICKUP FROM HERE
+    //FIND OUT THE INCLUDES FROM THE OTHER FILE AND ADD THEM IN HERE
+    //THERE ARE TWO OPTIONS FOR THE ALT ENTER. NOT SURE WHICH ONE IS RIGHT
+//    private void setupViewPager(ViewPager viewPager) {
+//        Adapter adapter = new Adapter(getSupportFragmentManager());
+//        adapter.addFragment(new CheeseListFragment(), "Category 1");
+//        adapter.addFragment(new CheeseListFragment(), "Category 2");
+//        adapter.addFragment(new CheeseListFragment(), "Category 3");
+//        viewPager.setAdapter(adapter);
+//    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) { super(fm); }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) { return mFragments.get(position); }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
 }
---
+++ mobile/ToolbarTest/app/src/main/java/com/example/emerson/toolbartest/SampleFragmentPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.example.emerson.toolbartest;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by emerson on 9/15/16.
+ */
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Home", "Events", "Social" };
+    private Context context;
+
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+
+}
