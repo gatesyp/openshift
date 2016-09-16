c0f573582c6b24c98a329679799758716a7cd7eb
russeii <rratcliffe57@gmail.com>
2016-09-16 09:34:08 -0400

    flask

--- flaskapp.py
+++ flaskapp.py
@@ -6,17 +6,18 @@ from flask import Flask, request, flash, url_for, redirect, \
 from engine.sql import SQLConnection
 from engine.plaid_test import plaid
 from engine.fitbit import fitbit
+from engine.social import social
 import schedule
 import time
 from threading import Thread
 
+
+#########ALL THREADS##############
 def MyThread1():
 	def call_fitbit_last_day():
 		fb=fitbit()
-		fb.add_events_fitness('rwr21')
+		fb.add_events_fitness()
 	    
-
-	
 	schedule.every().day.at("10:00").do(call_fitbit_last_day)
 	while 1:
 		 schedule.run_pending()
@@ -24,18 +25,29 @@ def MyThread1():
 t1=Thread(target=MyThread1,args=[])
 t1.start()
 
-def MyThread2():
-	def job1():
-	    print("I'm working...sdasdasdsd")
 
-	schedule.every(1).minutes.do(job1)
-	schedule.every().hour.do(job1)
-	schedule.every().day.at("10:30").do(job1)
+
+
+
+def MyThread2():
+	def call_si():
+	    si=social()
+	    social.calculate_social()
+
+	
+	schedule.every().day.at("1:00").do(call_si)
 	while 1:
 		 schedule.run_pending()
 		 time.sleep(1)
 t2=Thread(target=MyThread2,args=[])
 t2.start()
+
+
+
+
+
+
+#########END ALL THREADS##############
 
 app = Flask(__name__)
 app.config.from_pyfile('flaskapp.cfg')
@@ -61,7 +73,7 @@ def add_plaid(username):
 	#db=SQLConnection()
 	return 0232
 	p=plaid()
-	p.create_user_plaid("emc67","chase","rratcliffe57","***REMOVED***")
+	p.create_user_plaid("emc67","chase","rratcliffe57","roos_pass")
 	p.first_time_plaid_transactions("emc67")
 	return username
 
