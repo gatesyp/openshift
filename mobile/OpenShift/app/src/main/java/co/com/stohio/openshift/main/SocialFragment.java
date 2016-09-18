23d08018f7f95b06bf9bc9003e2fc81ac60d1809
emersoncloud <emerson.cloud@gmail.com>
2016-09-18 03:54:17 -0400

    Can no longer click on friends page

--- mobile/OpenShift/app/src/main/java/co/com/stohio/openshift/main/SocialFragment.java
+++ mobile/OpenShift/app/src/main/java/co/com/stohio/openshift/main/SocialFragment.java
@@ -2,6 +2,7 @@
 package co.com.stohio.openshift.main;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
